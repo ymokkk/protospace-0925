a8f8045a97e6fd923ca7a9c26863b1386f57831d
ymokkk <yamasaan126@gmail.com>
2022-04-11 23:52:17 +0900

    投稿の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,8 +4,22 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path(@prototype)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :detail, :point, :image, :category_id).merge(user_id: current_user.id)
+  end
+
 end
